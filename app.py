bbeaa014a8365245e74f2276a7df5df466cd72df
Alams4-hub <ayantayoalameen63@gmail.com>
2026-08-19 14:08:27 -0400

    used jinja template to send the task from python to html for it to display on the browser

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
-
+    task1 = ""
+    task2 = ""
+    task3 = ""
+    
+    if request.method == "POST":
+        task1 = request.form["task1"]
+        task2 = request.form["task2"]
+        task3 = request.form["task3"]
+        print(task1, task2, task3)
+    return render_template("index.html", task1=task1, task2=task2, task3=task3)
+ 
 
 if __name__ == "__main__":
     app.run(debug=True)
