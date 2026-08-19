555c29ae2c69b66541a78497d483640de8e4f280
Alams4-hub <ayantayoalameen63@gmail.com>
2026-08-18 23:12:20 -0400

    added templates/index.html and the render_template to app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Today's Three"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
